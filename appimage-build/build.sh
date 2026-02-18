3f54aee587c269fce262f6f8a48c0b4aad4c2df4
franckey02 <franckeyjavierc@gmail.com>
2026-02-18 18:35:28 -0400

    better foss

---
+++ appimage-build/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+rm -rf AppDir
+rm -f kdlplayer.desktop
+
+
+
+printf "[Desktop Entry]\nType=Application\nName=kdlplayer\nExec=kdlplayer\nIcon=kdlplayer\nCategories=AudioVideo;Player;\nTerminal=false\n" > kdlplayer.desktop
+
+cat kdlplayer.desktop
+
+./linuxdeploy-x86_64.AppImage \
+    --appdir AppDir \
+    --executable ./kdlplayer \
+    --desktop-file kdlplayer.desktop \
+    --icon-file icon/kdlplayer.png \
+    --library libs/libraylib.so.550 \
+    --output appimage
